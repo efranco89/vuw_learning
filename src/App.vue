1d424c9135c8415f0424c7d9a13c5c5aba952b36
Enrique Franco <luisfrancoz1@gmail.com>
2019-12-16 13:54:26 -0500

    First lesson done

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="app">
+    <p>Hello Vue World</p>
+    <h1>This is your first component in Vue</h1>
+    <h3>{{ message }}</h3>
+
+    <ul>
+      <li>{{ number + 1 }}</li>
+      <li>Value of ok var is: + {{ ok? 'true' : 'false' }}</li>
+      <li>{{ message.split('').reverse().join('') }}</li>
+    </ul>
+
+    <span v-if="visible">Now you can see me</span>
+
+    <ol>
+      <li v-for="todo in todos">{{ todo.text }}</li>
+    </ol>
+
+    <p>Original message: "{{ message }}"</p>
+    <p>Computed reversed message: "{{ reversedMessage}}"</p>
+  </div>
+</template>
+<script>
+  export default {
+    name: "App",
+    data: () => ({
+      message: "Powered by MDB!",
+      number: 5,
+      ok: true,
+      visible: true,
+      todos: [
+        { text: 'Learn JavaScript'},
+        { text: 'Learn Vue' },
+        { text: 'Build something awesome'}
+      ]
+    }),
+    computed: {
+      reversedMessage: function(){
+        return this.message.split("").reverse().join("");
+      }
+    }
+  };
+</script>
+<style scoped>
+  .app{
+    margin: 0 auto;
+    font-family: sans-serif;
+    background-color: #ccf7e2;
+    padding: 10px;
+    border-radius: 5px;
+    max-width: 500px;
+  }
+
+  div[class*=" col"]{
+    border: 1px dotted red;
+  }
+</style>
